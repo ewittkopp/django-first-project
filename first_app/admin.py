d6af1a4b55a135539123b6020bea1f819ecd7ac7
Ethan Wittkopp <ethan.wittkopp@ndsu.edu>
2026-04-22 11:52:37 -0500

    Added database support

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from first_app.models import Department, Employee
 
 # Register your models here.
+
+admin.site.register(Department)
+admin.site.register(Employee)
--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Department(models.Model):
+    dept_id = models.CharField(max_length=5, primary_key=True)
+    dept_name = models.CharField(max_length=20)
+    dept_location = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.dept_name
+    
+
+class Employee(models.Model):
+    emp_id = models.AutoField(primary_key=True)
+    emp_name = models.CharField(max_length=30)
+    emp_dept = models.ForeignKey(Department, on_delete=models.CASCADE)
+    emp_age = models.IntegerField()
+    emp_start_date = models.DateField()
+
+    def __str__(self):
+        return self.emp_name
