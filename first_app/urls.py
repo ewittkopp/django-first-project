1e173c9046b093d21c03a6a1eca69cc32300a8fb
Ethan Wittkopp <ethan.wittkopp@ndsu.edu>
2026-04-24 11:35:24 -0500

    Added full database support, employees page

--- first_app/urls.py
+++ first_app/urls.py
@@ -3,5 +3,6 @@ from first_app import views
 
 urlpatterns = [
 path('', views.index, name='index'),
+path('employees/', views.emp_view, name='employees'),
 path('about/', views.about, name='about'),
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Department, Employee
+
 # Create your views here.
 def index(request):
     dict = {
         'insert_me' : 'This is from views.py',
-        'fname' : 'Mike',
-        'lname' : 'Oldfield',
-        'age' : '60' 
+        'fname' : 'Ethan',
+        'lname' : 'Wittkopp',
+        'age' : '19' 
     }
     return render(request, 'first_app/index.html', context = dict)
     # return HttpResponse('Hello <a href="/about">World</a>')
+def emp_view(request):
+    employee_list = Employee.objects.order_by('emp_name')
+    emp_dict = {
+        'emp_list' : employee_list,
+    }
+    return render(request, 'first_app/employee.html', context = emp_dict)
 def about(request):
     return HttpResponse('<h1> This is the About page!</h1>')
